2a671a786f44d02f2cd95c84a5b031e0794bc19e
mrijal <mrijal110@gmail.com>
2026-04-25 03:00:47 +0700

    FIX | Turn of route register

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,13 @@ use App\Http\Controllers\StudentController;
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login')->middleware('guest');
 Route::post('/login', [AuthController::class, 'authenticate'])->middleware('guest');
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register')->middleware('guest');
-Route::post('/register', [AuthController::class, 'store'])->middleware('guest');
+// Route::get('/register', [AuthController::class, 'showRegister'])->name('register')->middleware('guest');
+// Route::post('/register', [AuthController::class, 'store'])->middleware('guest');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    
+
     Route::resource('departments', DepartmentController::class)->except(['create', 'edit', 'show']);
     Route::resource('courses', CourseController::class)->except(['create', 'edit', 'show']);
     Route::resource('students', StudentController::class)->except(['create', 'edit', 'show']);
